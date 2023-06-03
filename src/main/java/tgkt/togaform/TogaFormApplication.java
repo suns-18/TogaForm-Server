3943a0250354fd36fc9649dc1256ada746262b46
NBDatsuya <2646809050@qq.com>
2023-06-03 12:21:37 +0800

    init commit

---
+++ src/main/java/tgkt/togaform/TogaFormApplication.java
@@ -0,0 +1,13 @@
+package tgkt.togaform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogaFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogaFormApplication.class, args);
+    }
+
+}
