ad08cf84b732cc80418f25bde9256787c6fb89f1
NBDatsuya <2646809050@qq.com>
2023-06-07 10:52:21 +0800

    feat: UserMapper dao and xml

---
+++ src/main/java/tgkt/togaform/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package tgkt.togaform.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+import tgkt.togaform.entity.User;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface UserMapper {
+    List<User> queryList(User user);
+    List<User> verify(User user);
+    int insert(User user);
+    int update(User user);
+    int deleteById(User user);
+}
