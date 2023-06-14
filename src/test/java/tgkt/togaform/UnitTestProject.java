2e081812fd459751873dda922334780f8488c367
NBDatsuya <2646809050@qq.com>
2023-06-14 09:19:30 +0800

    feat: Project UnitTest class

---
+++ src/test/java/tgkt/togaform/UnitTestProject.java
@@ -0,0 +1,66 @@
+package tgkt.togaform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+import tgkt.togaform.controller.ProjectController;
+import tgkt.togaform.entity.ProjectInfo;
+import tgkt.togaform.service.ProjectService;
+import tgkt.togaform.util.UUIDUtil;
+
+@Component
+//@SpringBootTest
+public class UnitTestProject {
+	@Autowired
+	ProjectController projectController;
+	@Autowired
+	ProjectService projectService;
+
+	//@Test
+	public void controller(){
+
+		var pji = new ProjectInfo();
+		projectController.queryList(pji);
+
+		pji.setId("1");
+		projectController.queryList(pji);
+
+		pji.setId("0ff2e0a4e31d41439cd705b0eafc5f15");
+		projectController.queryList(pji);
+
+		pji.setId(null);
+		pji.setProjectName("k");
+		projectController.queryList(pji);
+
+		pji.setId(null);
+		pji.setProjectName(null);
+		projectController.queryList(pji);
+
+		pji.setId(UUIDUtil.getOneUUID());
+		pji.setProjectName("test");
+		projectController.addProject(pji);
+		projectController.addProject(pji);
+
+		pji.setProjectName("test1");
+		System.out.println(pji.toString());
+		projectController.updateUser(pji);
+
+		pji.setId(null);
+		pji.setProjectName(null);
+		projectController.updateUser(pji);
+
+		pji.setId("");
+		pji.setProjectName(null);
+		projectController.updateUser(pji);
+
+		pji.setId("114514");
+		pji.setProjectName(null);
+		projectController.updateUser(pji);
+
+		projectController.deleteUserById(pji);
+		projectController.deleteUserById(pji);
+		pji.setId(null);
+		projectController.deleteUserById(pji);
+	}
+}
