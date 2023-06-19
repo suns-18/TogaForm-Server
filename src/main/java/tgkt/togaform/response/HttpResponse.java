2d843514642fcdb3608ff4e719287ca746540541
NBDatsuya <2646809050@qq.com>
2023-06-19 12:20:56 +0800

    fix: resume lombok annotation

--- src/main/java/tgkt/togaform/response/HttpResponse.java
+++ src/main/java/tgkt/togaform/response/HttpResponse.java
@@ -1,40 +1,10 @@
 package tgkt.togaform.response;
 
+import lombok.Data;
+
+@Data
 public class HttpResponse {
     private String code;
     private Object data;
     private String message;
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    @Override
-    public String toString() {
-        return "HttpResponse{" +
-            "code='" + code + '\'' +
-            ", data=" + data +
-            ", message='" + message + '\'' +
-            '}';
-    }
 }
