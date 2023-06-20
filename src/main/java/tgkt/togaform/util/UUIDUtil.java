e63b865f7da9d446d825cc75bcd2e9daf9feb6dc
NBDatsuya <2646809050@qq.com>
2023-06-21 01:24:02 +0800

    style: delete javadocs

--- src/main/java/tgkt/togaform/util/UUIDUtil.java
+++ src/main/java/tgkt/togaform/util/UUIDUtil.java
@@ -5,21 +5,13 @@ import java.util.UUID;
 
 public class UUIDUtil {
     private UUIDUtil(){}
-    /**
-     * 获取一个UUID
-     */
+
     public static String getOneUUID() {
         //Get single UUID and replace "-" with ""
         return UUID.randomUUID().
                 toString().replaceAll("-", "");
     }
 
-    /**
-     * 获得指定数目的UUID
-     *
-     * @param count int 需要获得的UUID数量
-     * @return String[] UUID数组
-     */
     public static String[] getUUID(int count) {
         if (count < 1)
             return null;
