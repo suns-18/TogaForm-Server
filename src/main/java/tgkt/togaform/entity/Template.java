ad757cba5bf7145170d82df7095259776c4b032c
NBDatsuya <2646809050@qq.com>
2023-06-22 06:19:36 +0800

    feat: Template entity

--- src/main/java/tgkt/togaform/entity/Template.java
+++ src/main/java/tgkt/togaform/entity/Template.java
@@ -1,15 +1,18 @@
 package tgkt.togaform.entity;
 
 import java.util.Date;
+import java.util.List;
+
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document
 public class Template {
     private String id;
-
     private String title;
-
     private Date createTime;
-
     private String description;
+
+    private List<String> questions;
 }
