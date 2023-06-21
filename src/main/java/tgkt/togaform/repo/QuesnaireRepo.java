34acc03e7983ac06a745d774bfc447c864238b86
NBDatsuya <2646809050@qq.com>
2023-06-22 04:07:26 +0800

    feat: QuesnaireRepo

---
+++ src/main/java/tgkt/togaform/repo/QuesnaireRepo.java
@@ -0,0 +1,27 @@
+package tgkt.togaform.repo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import tgkt.togaform.entity.Quesnaire;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface QuesnaireRepo extends
+        MongoRepository<Quesnaire, String> {
+    @Override
+    <S extends Quesnaire> S insert(S entity);
+    @Override
+    void deleteById(String s);
+    @Override
+    <S extends Quesnaire> S save(S entity);
+
+    Optional<Quesnaire> findById(String Id);
+    List<Quesnaire> findByProject(String projectId);
+
+    @Override
+    boolean existsById(String s);
+    @Override
+    long count();
+}
