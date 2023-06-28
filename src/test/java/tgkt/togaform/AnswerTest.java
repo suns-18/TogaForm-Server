6a645cc700b6fed79470ad72d10d76ee174fae91
wangwenqingqq <1064078316@qq.com>
2023-06-28 23:01:34 +0800

    fix:AnswerTest

--- src/test/java/tgkt/togaform/AnswerTest.java
+++ src/test/java/tgkt/togaform/AnswerTest.java
@@ -24,20 +24,6 @@ public class AnswerTest {
     @Test
     void query() {
         var a = new AnswerListRequest();
-        a.setUserId("admin");
-        Assertions.assertFalse(((List<Answer>)
-                        (controller.queryByUser(a)
-                                .getData()))
-                        .isEmpty()
-                , "Answer模块>>列表请求测试1：根据用户ID查询，未通过");
-        log.info("Answer模块>>列表请求测试1：根据用户ID查询，通过");
-        a.setQuesnaire("nihao");
-        Assertions.assertFalse(((List<Answer>)
-                        (controller.queryByProject(a)
-                                .getData()))
-                        .isEmpty()
-                , "Answer模块>>列表请求测试1：根据问卷查询，未通过");
-        log.info("Answer模块>>列表请求测试1：根据问卷查询，通过");
         a.setId("guagua");
         Assertions.assertFalse(((List<Answer>)
                         (controller.queryById(a)
@@ -61,17 +47,17 @@ public class AnswerTest {
         var a = new Answer();
         a.setQuesnaire("test1");
         Assertions.assertEquals(
-                controller.add(a).getCode(), "1");
+                controller.add(a).getCode(), 1);
         log.info("Answer模块>>添加请求测试1：正常情况，通过");
 
         a.setQuesnaire(null);
         Assertions.assertEquals(
-                controller.add(a).getCode(), "0");
+                controller.add(a).getCode(), 0);
         log.info("Answer模块>>添加请求测试2：问卷名为null，通过");
 
         a.setQuesnaire("");
         Assertions.assertEquals(
-                controller.add(a).getCode(), "0");
+                controller.add(a).getCode(), 0);
         log.info("Answer模块>>添加请求测试3：问卷名为空字符串，通过");
 
         log.info("Answer模块>>添加请求测试通过");
@@ -82,25 +68,25 @@ public class AnswerTest {
         a = (AnswerListRequest) ((List<?>) (controller.queryById(a).getData()));
         a.setId("123");
         Assertions.assertEquals(
-                controller.modify(a).getCode(), "1");
+                controller.modify(a).getCode(), 1);
         log.info("Answer模块>>更新请求测试1：正常情况，通过");
 
         a.setId(null);
         a.setQuesnaire(null);
         Assertions.assertEquals(
-                controller.modify(a).getCode(), "0");
+                controller.modify(a).getCode(), 0);
         log.info("Answer模块>>更新请求测试2：Id和所修改字段新值均为null，通过");
 
         a.setId("");
         a.setQuesnaire(null);
         Assertions.assertEquals(
-                controller.modify(a).getCode(), "0");
+                controller.modify(a).getCode(), 0);
         log.info("Answer模块>>更新请求测试3：Id为空串，所修改字段新值为null，通过");
 
         a.setId("114514");
         a.setQuesnaire(null);
         Assertions.assertEquals(
-                controller.modify(a).getCode(), "0");
+                controller.modify(a).getCode(), 0);
         log.info("Answer模块>>更新请求测试4：不存在的Id，通过");
 
         log.info("Answer模块>>更新请求测试通过");
@@ -111,18 +97,19 @@ public class AnswerTest {
         var a = new AnswerListRequest();
         a.setId(null);
         Assertions.assertEquals(
-                controller.deleteById(a).getCode(), "0");
+                controller.deleteById(a).getCode(), 0);
         log.info("Answer模块>>删除请求测试1：Id为null，通过");
 
         a.setQuesnaire("test");
-        a = ((List<AnswerListRequest>) controller.queryByProject(a).getData()).get(0);
+        var aList =controller.queryById(a).getData();
+        a = ((List<AnswerListRequest>) controller.queryById(a).getData()).get(0);
         Assertions.assertEquals(
-                controller.deleteById(a).getCode(), "1");
+                controller.deleteById(a).getCode(), 1);
         log.info("Answer模块>>删除请求测试2：正常情况，通过");
 
         a.setId("oooooooooooooooooooooooooooooooo");
         Assertions.assertEquals(
-                controller.deleteById(a).getCode(), "0");
+                controller.deleteById(a).getCode(), 0);
         log.info("Answer模块>>删除请求测试3：不存在的Id，通过");
 
         log.info("Answer模块>>删除请求测试通过");
